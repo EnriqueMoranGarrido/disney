8d62560a6de8dcfb048f7877d3c036a268bb0d9a
EnriqueMoranGarrido <enrique.morango@udlap.mx>
2022-06-21 21:44:24 -0500

    Hero land page for login and navbar fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import Hero from "../components/Hero";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Navbar />
+      <Hero />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
