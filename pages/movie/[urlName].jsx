c9d2d3ee74b21fe276c4b1c5f021eeb5013f187b
Enrique Moran <enrique.morango@udlap.mx>
2022-06-28 21:05:27 -0500

    created dynamic page with movie selection! I'm so happy lol

--- pages/movie/[urlName].jsx
+++ pages/movie/[urlName].jsx
@@ -1,10 +1,12 @@
+import React from "react";
+
 import { getSession, useSession } from "next-auth/react";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-import React from "react";
+import marvelMovies from "../../public/assets/data/marvelMovies";
 
 function Movie({ movie }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
@@ -12,7 +14,7 @@ function Movie({ movie }) {
   const [showPlayer, setShowPlayer] = useState(false);
   const { data: session } = useSession();
 
-  console.log(movie);
+  console.log(movie.name);
   useEffect(() => {
     if (!session) {
       router.push("/");
@@ -21,12 +23,38 @@ function Movie({ movie }) {
   return (
     <div className="relative">
       <Head>
-        <title>{movie}</title>
+        <title>{movie.name}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      [urlName]
+      <p>{"hola"}</p>
     </div>
   );
 }
 
 export default Movie;
+
+export async function getServerSideProps(context) {
+  const findMyMovie = (element) => element.urlName == movieUrlName;
+
+  const session = await getSession(context);
+
+  const movieUrlName = context.query.urlName;
+  const movieId = marvelMovies.findIndex(findMyMovie);
+  const movie = marvelMovies[movieId];
+  console.log(movie);
+  // const { id } = context.query;
+  // const movie = marvelMovies[context.query.id];
+  // console.log(movie);
+
+  // const request = await fetch(
+  //   `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
+  // ).then((response) => response.json());
+
+  return {
+    props: {
+      session,
+      movie,
+      // result: request,
+    },
+  };
+}
